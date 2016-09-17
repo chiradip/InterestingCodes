84ea4013ec4af7a4036cad10cec497efb3de249a
Chiradip Mandal <chiradip@chiradip.com>
2016-09-17 07:17:50 -0700

    Update fact-fibs.clj

--- fact-fibs.clj
+++ fact-fibs.clj
@@ -9,7 +9,10 @@
     (if (> x 0)
       (recur (dec x) curr-fib (+ p-fib curr-fib)) p-fib))))
 
-
+;; This one will produce a collection of fibonacci series which can be used to do other opertions of the series 
+;; without computing them individually all over again just provide snything as the second argument and it will produce
+;; the fibonacci series in list form in reverse order - use vector as the storing mechsnism to get the series in regular 
+;; order
 (defn fib-recur
 [num & store]
   (loop [x num p-fib 0 curr-fib 1 l '()]
