7fcf0a060933d6dc3f0038c3896e1c494c13ce15
Chiradip Mandal <chiradip@chiradip.com>
2016-11-06 12:04:32 -0800

    Update scribble.clj

--- scribble.clj
+++ scribble.clj
@@ -18,3 +18,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defproject my-app "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot my-app.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
