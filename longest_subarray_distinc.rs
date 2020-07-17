01827884c867caa63cc24c7868bad817762b9623
Chiradip Mandal <chiradip@chiradip.com>
2020-07-17 10:16:49 -0700

    Update longest_subarray_distinc.rs

--- longest_subarray_distinc.rs
+++ longest_subarray_distinc.rs
@@ -1,19 +1,19 @@
-pub fn longest_subarray_distinct(arr: &[i32]) -> i32 {
+pub fn longest_subarray_distinct(arr: &[i32]) -> usize {
     let mut window_start :usize = 0;
     let mut window_content : HashMap<i32,usize> = HashMap::new(); //with_capacity(100); // HashMap::new();
 
-    let mut longest= 0;
+    let mut longest :usize= 0;
 
     for window_end in 0..arr.len() {
         let key_option = window_content.get(&arr[window_end]);
         window_start = match key_option {
             Some(value) =>
-                max(window_start, value+1),
+                max(window_start,value+1),
             None => window_start // println!("No value for key = {}", &arr[window_end])
         };
         println!("Window Start = {}, Window End = {}", window_start, window_end);
         longest = max(longest, window_end - window_start + 1);
         window_content.insert(arr[window_end], window_end);
     }
-    longest as i32
+    longest
 }
