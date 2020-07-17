2c033f8261301894936f4641d8e333d75c616b9d
Chiradip Mandal <chiradip@chiradip.com>
2020-07-16 22:43:54 -0700

    Update longest_subarray_distinc.rs

--- longest_subarray_distinc.rs
+++ longest_subarray_distinc.rs
@@ -4,14 +4,14 @@ pub fn longest_subarray_distinct(arr: &[i32]) -> i32 {
 
     let mut longest= 0;
 
-    for window_end in 0..arr.len()-1 {
+    for window_end in 0..arr.len() {
         let key_option = window_content.get(&arr[window_end]);
         window_start = match key_option {
-            Some(value) => {
-                return max(window_start, value+1) as i32;
-            },
+            Some(value) =>
+                max(window_start, value+1),
             None => window_start // println!("No value for key = {}", &arr[window_end])
         };
+        println!("Window Start = {}, Window End = {}", window_start, window_end);
         longest = max(longest, window_end - window_start + 1);
         window_content.insert(arr[window_end], window_end);
     }
